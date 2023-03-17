0c021ff23091f1f84c3b51c659a14d35fb3ecc53
fuadmoin <fuadamoin@gmail.com>
2023-03-17 16:54:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
-/* eslint-disable */
-/*import './style.css';
-import Api from './modules/api.js';
-import refresh from './modules/refresh.js';
-
-const request = new Api();
-
-const displayData = async () => {
-  const data = await request.getScore();
-  refresh(data);
-};
-
-displayData();
-
-document.getElementById('refresh-btn').addEventListener('click', async () => {
-  displayData();
-});
-
-document.getElementById('form').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const form = document.getElementById('form');
-  const {elems} = form1;
-  const data = {
-    user: elems.user.value,
-    score: elems.score.value,
-  };
-
-  await request.addScore(data);
-  form.reset();
-  displayData();
-}); */
-
-
 
 import './style.css';
-import Api from './modules/game-api.js';
-import refresh from './modules/display';
+import Api from './modules/api.js';
+import refresh from './modules/display.js';
 
 const request = new Api();
 
@@ -59,5 +26,4 @@ document.querySelector('#score-form').addEventListener('submit', async (e) => {
   };
   await request.addScore(data);
   els.reset();
-  displayData();
 }); 
